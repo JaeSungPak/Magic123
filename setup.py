0f028cce20447b0226aeb1e7e7a112d611447d2a
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-08-28 16:24:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="shap-e",
+    name="magic123",
     packages=[
-        "shap_e",
-        "shap_e.diffusion",
-        "shap_e.models",
-        "shap_e.models.generation",
-        "shap_e.models.nerf",
-        "shap_e.models.nerstf",
-        "shap_e.models.nn",
-        "shap_e.models.stf",
-        "shap_e.models.transmitter",
-        "shap_e.rendering",
-        "shap_e.rendering.blender",
-        "shap_e.rendering.raycast",
-        "shap_e.util",
+        "nerf",
     ],
     install_requires=[
         "filelock",
